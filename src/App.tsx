421a362e0e2904858a1c9477bc393407700ccd33
simo54 <65959785+simo54@users.noreply.github.com>
2022-10-30 10:52:11 +0100

    Update App.tsx
    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ import { Class } from '../node_modules/node-test-pckg/dist/main';
 function App() {
 	const test = number;
 	const anotherTest: IStuff = { first: 'Nice', second: 'Test' };
-	/** This class init */
 	const person = new Class('jake');
-	console.log(test);
 	return (
 		<>
 			<p>
